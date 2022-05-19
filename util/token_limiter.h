e3cb4c799369cbe473a0e92a7bbe9f327012ec5d
wangnengjie <751614701@qq.com>
2022-05-19 19:11:44 +0800

    fix bug cause by env_->NowNanos() != env_->NowMicros() * 1000

--- util/token_limiter.h
+++ util/token_limiter.h
@@ -23,10 +23,8 @@ class TokenLimiter {
   class Req {
    public:
     port::CondVar cv_;
-    uint64_t enter_ns_;
     bool granted_;
-    explicit Req(port::Mutex* mu, uint64_t enter_ns)
-        : cv_(mu), enter_ns_(enter_ns), granted_(false) {}
+    explicit Req(port::Mutex* mu) : cv_(mu), granted_(false) {}
   };
 
  private:
@@ -36,10 +34,10 @@ class TokenLimiter {
 
   int32_t available_tokens_;
   uint64_t next_refill_sec_;
-  // 900 * 1000000, 700 * 1000000, 500 * 1000000, 0
+  // 900 * 1000, 700 * 1000, 500 * 1000, 0
   //
   // Prefetch, Compaction, Flush, User
-  uint64_t wait_threshold_ns_[Env::IOSource::IO_SRC_DEFAULT];
+  uint64_t wait_threshold_us_[Env::IOSource::IO_SRC_DEFAULT];
   uint64_t total_requests_[IOType::kWrite + 1]
                           [Env::IOSource::IO_SRC_DEFAULT + 1];
 
@@ -61,6 +59,7 @@ class TokenLimiter {
   explicit TokenLimiter(int32_t tokens_per_sec);
   ~TokenLimiter();
   void RequestToken(Env::IOSource io_src, IOType io_type);
+  static std::string IOSourceToString(Env::IOSource io_src);
 
  private:
   // lock must hold before calling this function.
@@ -68,18 +67,8 @@ class TokenLimiter {
   // calc wait time point.
   uint64_t CalcWakeMicros();
   // dispatch token to req in queue
-  void DispatchToken(uint64_t now_ns);
+  void DispatchToken(uint64_t now_us);
   void TunePrefetch();
-
- private:
-  static uint64_t NowSec(Env* env) { return env->NowNanos() / std::nano::den; }
-  static uint64_t NowSecInNanos(Env* env) {
-    return TokenLimiter::NowSec(env) * std::nano::den;
-  }
-  static uint64_t NowMicros(Env* env) {
-    return env->NowNanos() / std::micro::den;
-  }
-  static std::string IOSourceToString(Env::IOSource io_src);
 };
 
 }  // namespace rocksdb
